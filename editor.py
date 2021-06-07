32776134ea7d5ffc5963f033fd85d597e05c25ff
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-06-07 15:18:48 +0530

    save as implement

--- editor.py
+++ editor.py
@@ -2,6 +2,21 @@ import tkinter as tk
 
 from tkinter.filedialog import *
 
+def saveFile():
+    fileLocation = asksaveasfilename(
+        defaultextension = "txt",
+        filetypes=[("Text Files","*.txt"),("All Files","*.*")]
+    )
+
+    if not fileLocation:
+        return
+    # else:
+    #     print(fileLocation)
+    with open(fileLocation,"w") as ofile:  # File handling with open() => Opens file
+        notes = text_editor.get(1.0,tk.END)
+        ofile.write(notes)
+    window.title(fileLocation + " - Zorro Editor")
+
 window = tk.Tk()  # Initializes Tkinter window
 
 window.title("Zorro Editor")  # Gives window a Title
@@ -18,7 +33,7 @@ tools.grid(row=0, column=0, sticky="ns")
 open_button = tk.Button(tools, text="Open File")
 open_button.grid(row=0, column=0, padx=3, pady=3)
 
-save_button = tk.Button(tools, text="Save As")
+save_button = tk.Button(tools, text="Save As",command=saveFile)
 save_button.grid(row=0, column=1, padx=3, pady=3)
 
 window.mainloop()
