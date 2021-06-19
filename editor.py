e3d954d1f3b4364db0270f27b05a00e6760cd69a
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-06-19 12:17:06 +0530

    pack

--- editor.py
+++ editor.py
@@ -46,23 +46,26 @@ window = tk.Tk()  # Initializes Tkinter window
 
 window.title("Zorro Editor")  # Gives window a Title
 
-window.rowconfigure(0, minsize=600)
-window.columnconfigure(1, minsize=600)
-
+window.rowconfigure(0, minsize=500)
+window.columnconfigure(1, minsize=500)
+tools = tk.Frame(window)
+# tools.grid(row=0, column=0, sticky="ns")
+tools.pack(expand=True)
 text_editor = tk.Text(window)
-text_editor.grid(row=0, column=1, sticky="nsew")
-
-tools = tk.Frame(window, relief=tk.RAISED, bd=2)
-tools.grid(row=0, column=0, sticky="ns")
+# text_editor.grid(row=0, column=1, sticky="nsew")
+text_editor.pack()
 
 open_button = tk.Button(tools, text="Open File", command=openFile)
 open_button.grid(row=0, column=0, padx=3, pady=3)
+# open_button.pack()
 
 saveas_button = tk.Button(tools, text="Save As", command=saveAsFile)
 saveas_button.grid(row=0, column=1, padx=3, pady=3)
+# saveas_button.pack()
 
 save_button = tk.Button(tools, text="Save", command=saveFile)
-save_button.grid(row=1, column=0)
+save_button.grid(row=0, column=2,padx=3, pady=3)
+# save_button.pack()
 
 window.bind("<Control-o>",lambda x:openFile())
 window.bind("<Control-s>",lambda x:saveFile())
